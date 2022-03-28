f49a1cec8c734630691ddce894f5b78ba4723dca
Sergio Andres Mona Monsalve <sergiomona@seti.com.co>
2022-03-27 21:22:08 -0500

    Se agrega funcionalidad al componente de Registrar Vehiculos

--- src/app/componentes/registro-vehiculos/registro-vehiculos.component.ts
+++ src/app/componentes/registro-vehiculos/registro-vehiculos.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Vehiculos } from './../../models/vehiculos.models'
+import { TipoVehiculo } from './../../models/tipoVehiculos.enum';
+import { GlobalController } from './../../models/global';
+
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-registro-vehiculos',
@@ -7,9 +12,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroVehiculosComponent implements OnInit {
 
-  constructor() { }
+  public vehiculo: Vehiculos = new Vehiculos();
+  public tipoVehiculo = TipoVehiculo;
+  public successFull: boolean = false;
+
+
+  constructor(public variablesGlobales: GlobalController) { }
 
   ngOnInit(): void {
+  }
+
+  cargarVehiculo(e: NgForm) {
+    let idx = this.variablesGlobales.vehiculosReg.findIndex(item => item.Placa.toUpperCase() == this.vehiculo.Placa.toUpperCase());
+
+    if(idx >= 0) {
+      alert("El vehiculo ya está registrado.");
+      return;
+    }
+    
+    this.vehiculo.Placa = this.vehiculo.Placa.toUpperCase();
+    this.variablesGlobales.vehiculosReg.push(this.vehiculo);
+    this.clear(e);
+    this.successFull = true;
+  }
+
+  clear(e: NgForm) {
+    this.vehiculo = new Vehiculos();
+    e.reset();
+    this.successFull = false;
+    this.vehiculo.Tipo = this.tipoVehiculo.Carro;
+
+  }
+
+  successrefresh() {
+    this.successFull = false;
   }
 
 }
--- src/app/models/vehiculos.models.ts
+++ src/app/models/vehiculos.models.ts
@@ -1,9 +1,9 @@
 import { TipoVehiculo } from './tipoVehiculos.enum';
 export class Vehiculos {
 constructor(
-    public Placa: string,
-    public Propietario: string,
-    public Tipo: TipoVehiculo
+    public Placa: string = "",
+    public Propietario: string = "",
+    public Tipo: TipoVehiculo = TipoVehiculo.Carro
 ) {}
 
 }
